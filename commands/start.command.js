09d91fddbc033666b38dd7cf5860b85bc9eb2c13
Akbar Jorayev <akbarjonjorayev001@gmail.com>
2026-02-06 21:21:05 +0500

    punctuation fixed

--- commands/start.command.js
+++ commands/start.command.js
@@ -1,5 +1,5 @@
 async function startCommand(ctx) {
-  const text = `Salom1, ${ctx.from.first_name}! \n\nMen Poktil, hoziroq meni guruhga qo'shing va men uni nazorat qilaman.`
+  const text = `Salom, ${ctx.from.first_name}! \n\nMen Poktil, hoziroq meni guruhga qo'shing va men uni nazorat qilaman.`
   await ctx.reply(text)
 }
 
